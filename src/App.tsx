ec05207495f804a85b90d6e783909878083ab9c3
ThomasLavallee <lavallee@udel.edu>
2025-03-25 18:19:22 -0400

    Switched routing to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Homepage } from './pages/Homepage';
 import {BasicQuiz} from './pages/BasicQuiz'
 import { DetailedQuiz } from './pages/DetailedQuiz';
@@ -9,13 +9,13 @@ function App(): React.JSX.Element {
   // Routing info to go to the correct page
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
           <Route path="/BasicQuiz" element={<BasicQuiz />}></Route>
           <Route path="/DetailedQuiz" element={<DetailedQuiz />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
