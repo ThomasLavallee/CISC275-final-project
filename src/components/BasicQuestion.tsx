f362ee6234f28b3854bac9355071c690df5ed6b8
Matthew Stone <mbstone@udel.edu>
2025-03-31 15:58:38 -0400

    Started basic questions

---
+++ src/components/BasicQuestion.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import './ComponentStyles.css'
+import { Link } from "react-router-dom";
+
+interface BasicQuestionProps {
+    quizType: "BasicQuiz" | "DetailedQuiz",
+    description: string[]
+  }
+
+export function BasicQuestion({quizType, description}: BasicQuestionProps): React.JSX.Element {
+    // Format the quiz title
+    const regex = new RegExp(`(\\Q)`, 'g'); 
+    const formattedTitle: string = quizType.replace(regex, ` $1`);;
+
+    return <div className="Quiz-Description">
+        <h4>{formattedTitle}:</h4>
+        <div className="Description-Container">
+            <ul>
+            {
+                description.map((sentence: string) => {
+                    return <li>{sentence}</li>
+                })
+            }
+            </ul>
+        </div>
+        <Link to={`/${quizType}`}>
+            <Button>Begin <br></br>{formattedTitle}</Button>
+        </Link>
+    </div>
+}
--- src/pages/BasicQuiz.tsx
+++ src/pages/BasicQuiz.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export function BasicQuiz(): React.JSX.Element {
+
+    const [questionNumber, setQuestionNumber] = useState<number>(1)
+
     return <div>
         <Link to={"/"}>
            <Button>Home</Button>
         </Link>
         Basic Quiz
+        <br></br>
+        <Button onClick={() => {setQuestionNumber(questionNumber-1)}} disabled={questionNumber===1}>Previous</Button>
+        <Button onClick={() => {setQuestionNumber(questionNumber+1)}} disabled={questionNumber===10}>Next</Button>
+        <div>{questionNumber}</div>
     </div>
 }
