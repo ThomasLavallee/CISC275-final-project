2af421a74e05b3ccc45678ab15f999018004f4d1
Matthew Stone <mbstone@udel.edu>
2025-04-07 10:55:24 -0400

    Detailed Question Page Made

--- src/pages/DetailedQuiz.tsx
+++ src/pages/DetailedQuiz.tsx
@@ -1,13 +1,43 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { DetailedQuestion } from '../components/DetailedQuestion';
+import { Button } from 'react-bootstrap';
+
 
 export function DetailedQuiz(): React.JSX.Element {
-    return <div>
-        Detailed Quiz
+    const [questionNumber, setQuestionNumber] = useState<number>(1)
 
-        <Link to={"/"}>
+    // All ten of the Detailed questions given in this quiz
+    const DetailedQuestions = ["Favorite Color?", "Favorite Food?"]
+    // All ten sets of answer choices for the questions in this quiz
+    const DetailedOptions = [["red", "green", "blue"], ["apple", "beans", "cat"]]
+    
+    // Number of questions
+    const numQuestions: number = DetailedQuestions.length;
+
+    return <div className="Detailed-Quiz-Page">
+        
+
+        <div className="Detailed-Page-Navigation-Buttons">
+            <Link to={"/"}>
             <Button>Home</Button>
-        </Link>
+            </Link>
+
+            <Button disabled={true}>Detailed Quiz</Button>
+
+        </div>
+        
+        <br></br>
+        <div className="Detailed-Quiz-Question-Wrapper">
+            <br></br>
+            <DetailedQuestion questionNumber={questionNumber} question={DetailedQuestions[questionNumber-1]} options={DetailedOptions[questionNumber-1]}></DetailedQuestion>
+            <br></br>
+            <Button id="Detailed-Quiz-Prev-Next-Button" onClick={() => {setQuestionNumber(questionNumber-1)}} disabled={questionNumber===1}>Previous</Button>
+            <br></br>
+            <Button id="Detailed-Quiz-Prev-Next-Button" onClick={() => {setQuestionNumber(questionNumber+1)}} disabled={questionNumber===numQuestions}>Next</Button>
+        </div>
+        <br></br>
+        <Button className="Detailed-Quiz-Navigation-Button" disabled={questionNumber!==numQuestions}>Get Results</Button>
+
     </div>
 }
