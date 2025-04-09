2c393c716653ae2e8627654b800df03c6285ca4a
Matthew Stone <mbstone@udel.edu>
2025-04-09 16:04:13 -0400

    Compiles answers for basic quiz into full string of questions and answers

--- src/pages/BasicQuiz.tsx
+++ src/pages/BasicQuiz.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { BasicQuestion } from '../components/BasicQuestion';
+import { BasicQuestion} from '../components/BasicQuestion';
 import { Button } from 'react-bootstrap';
 import './BasicQuizComponentStyles.css';
 
 
 export function BasicQuiz(): React.JSX.Element {
-    const [questionNumber, setQuestionNumber] = useState<number>(1)
-
     // All ten of the basic questions given in this quiz
     const basicQuestions = ["Do you like working with people?", "Would you rather design or build something?", "Do you like working creatively?","Do you like leading others?","Do you prefer routine or variety at work?","Do you like working in hands-on environments?","Are you comfortable using technology?","Do you enjoy writing?","Would you rather work alone or in a team?","Do you enjoy public speaking?"]
     // All ten sets of answer choices for the questions in this quiz
-    const basicOptions = [["Yes, all the time","Sometimes","Not really","Only in small groups"], ["Design","Build","Both","Neither"],["All the time","Occasionally","Not much","Only when necessary"],["Yes","Sometimes","Not really","Only if no one else will"],["Routine","Variety","A balance","I'm not sure"],["Yes","Sometimes","Not really","Only for hobbies"],["Very comfortable","Somewhat comfortable","Not really","I avoid it"],["Yes","A little","Not much","I dislike it"],["Alone","In a team","Either","Depends on the task"],["Yes","Sometimes","Not really","I avoid it"]]
-    
+    const basicOptions = [["","Yes, all the time","Sometimes","Not really","Only in small groups"], ["","Design","Build","Both","Neither"],["","All the time","Occasionally","Not much","Only when necessary"],["","Yes","Sometimes","Not really","Only if no one else will"],["","Routine","Variety","A balance","I'm not sure"],["","Yes","Sometimes","Not really","Only for hobbies"],["","Very comfortable","Somewhat comfortable","Not really","I avoid it"],["","Yes","A little","Not much","I dislike it"],["","Alone","In a team","Either","Depends on the task"],["","Yes","Sometimes","Not really","I avoid it"]]
     // Number of questions
     const numQuestions: number = basicQuestions.length;
+    // State :)
+    const [questionNumber, setQuestionNumber] = useState<number>(1)
+    const [answers, setAnswers] = useState<string[]>(Array(numQuestions).fill(""));
+    // Full string of questions and answers
+    let basicReport: string = ""
+    for (let i = 0; i < numQuestions; i++) {
+        basicReport += basicQuestions[i] + " " + answers[i] + ".\n"
+    }
+    if (basicReport) {}
 
     return <div className="Basic-Quiz-Page">
-        
-
         <div className="Basic-Page-Navigation-Buttons">
             <Link to={"/"}>
             <Button>Home</Button>
@@ -31,7 +35,12 @@ export function BasicQuiz(): React.JSX.Element {
         <br></br>
         <div className="Basic-Quiz-Question-Wrapper">
             <br></br>
-            <BasicQuestion questionNumber={questionNumber} question={basicQuestions[questionNumber-1]} options={basicOptions[questionNumber-1]}></BasicQuestion>
+            <BasicQuestion questionNumber={questionNumber} question={basicQuestions[questionNumber-1]} options={basicOptions[questionNumber-1]} selected={answers[questionNumber-1]} 
+            onSelect={(qn, selectedAnswer) => {
+                const updatedAnswers = [...answers];
+                updatedAnswers[qn - 1] = selectedAnswer;
+                setAnswers(updatedAnswers)
+            }}></BasicQuestion>
             <br></br>
             <Button id="Basic-Quiz-Prev-Next-Button" onClick={() => {setQuestionNumber(questionNumber-1)}} disabled={questionNumber===1}>Previous</Button>
             <br></br>
